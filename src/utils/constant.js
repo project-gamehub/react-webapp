aecde67daef3fcf03896c00440fd0efa4c9c0018
Awez Mirza <awezbaigmirza@gmail.com>
2024-12-08 22:37:50 +0530

    Updated constants

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-const API_GATEWAY_URL = "http://65.0.20.85:3007";
+const API_GATEWAY_URL = "https://game-hub.duckdns.org/backend/api";
 
 export const USER_SERVICE_URL = `${API_GATEWAY_URL}/user`;
 export const FRIENDS_SERVICE_URL = `${API_GATEWAY_URL}/friend`;
@@ -6,8 +6,10 @@ export const GAMES_SERVICE_URL = `${API_GATEWAY_URL}/games`;
 export const CHAT_SERVICE_URL = `${API_GATEWAY_URL}/chat`;
 export const TICTACTOE_SERVICE_URL = `${API_GATEWAY_URL}/tic-tac-toe`;
 
-export const CHAT_SERVICE_SOCKET_URL = "http://65.0.20.85:3005";
-export const TICTACTOE_SOCKET_URL = "http://65.0.20.85:3006";
+export const CHAT_SERVICE_SOCKET_URL =
+    "https://game-hub.duckdns.org/backend/chat";
+export const TICTACTOE_SOCKET_URL =
+    "https://game-hub.duckdns.org/backend/tictactoe";
 
 export const LOGO_URL = "https://i.postimg.cc/SQVJXt7K/logo.png";
 
@@ -17,4 +19,5 @@ export const NO_BANNER_FOUND_URL =
 export const GOOGLE_CLIENT_ID =
     "99839884324-eps1tje234akps84633996ch9r5l7kne.apps.googleusercontent.com";
 
-export const LOGIN_PAGE_BANNER_URL = "https://i.ibb.co/myDQjv3/login-banner.jpg";
+export const LOGIN_PAGE_BANNER_URL =
+    "https://i.ibb.co/myDQjv3/login-banner.jpg";
